44520f89fff91f289eb3440d309c951b23504b9a
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2023-06-11 19:57:29 +0200

    chore: use new `snapshotPathTemplate` config
    instead of _autoSnapshotSuffix workaround (https://github.com/microsoft/playwright/issues/11134)

--- src/globals.ts
+++ src/globals.ts
@@ -2,17 +2,8 @@ import { test as base } from "@playwright/test";
 import * as sinon from "sinon";
 
 export const test = base.extend<{
-  _autoSnapshotSuffix: void;
   fakeClock: sinon.SinonFakeTimers;
 }>({
-  _autoSnapshotSuffix: [
-    async ({}, use, testInfo) => {
-      testInfo.snapshotSuffix = "";
-      await use();
-    },
-    { auto: true },
-  ],
-
   fakeClock: [
     async ({}, use) => {
       const clock = sinon.useFakeTimers();
